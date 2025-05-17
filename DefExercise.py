111bcdd3aa7621aaeb9c833f187fca028c79b76f
butler-kor <youinmok@daum.net>
2025-05-17 19:22:36 +0900

    def에 리스트로 넣는 것은 되는데...
    def Score(no1, no2, no3) 일때
    
    Score([9,9,9,9],[9,9,9,9],[9,9,9,9])는 되는데
    리스트로 만든
    no1Student = [9,9,9,9]
    no2Student = [9,9,9,9]
    no3Student = [9,9,9,9]
    
    Score = (no1Student, no2Student, no3Student)
    가 안되는 이유가 뭘까????????

--- DefExercise.py
+++ DefExercise.py
@@ -23,29 +23,34 @@ from zstandard.backend_cffi import STRATEGY_BTLAZY2
 
 #def를 이용하여 성적리스트를 받고 평균 값을 출력
 def Score(no1,no2,no3):
-    num1 = 0
-    num2 = 0
-    num3 = 0
-    num4 = 0
-    while num1 < len(no1):
-        num2 = no1[num1]
-        num3 = no2[num1]
-        num4 = no3[num1]
-        num1 = num1 + 1
-    num2 = num2 / len(no1) + 1
-    num3 = num3 / len(no2) + 1
-    num4 = num4 / len(no3) + 1
-    return num2,num3,num4
+   num1 = sum(no1) / len(no1)
+   num2 = sum(no2) / len(no2)
+   num3 = sum(no3) / len(no3)
+   return num1,num2,num3
 
 List1 = ['수학','영어','과학','물리']
 List2 = []
 num1 = 0
 num2 = 0
 num3 = 0
+no1Student = []
+no2Student = []
+no3Student = []
 for i in range(1,4):
+    num2 = num2 + 1
     List2 = []
-    print(f"{num1 + 1}번째 학생의 점수를 입력해 주세요")
-    for j in len(List1):
-        num1 = int(input(f"{num1 + 1}번째 학생의 {List1[num1]} 점수를 입력해주세요:"))
+    print(f"{num2}번째 학생의 점수를 입력해 주세요")
+    num3 = 0
+    for j in range(len(List1)):
+        num1 = int(input(f"{num2}번째 학생의 {List1[num3]} 점수를 입력해주세요:"))
         List2.append(num1)
-    Score().append(List2)
+        num3 = num3 + 1
+    if num2 == 1:
+        no1Student.append(List2)
+    elif num2 == 2 :
+        no2Student.append(List2)
+    elif num2 == 3 :
+        no3Student.append(List2)
+print(no1Student)
+print(no2Student)
+print(no3Student)
