3733cb8d9bd5b116bcfee1132ae0d7a124c64841
butler-kor <youinmok@daum.net>
2025-06-12 08:25:05 +0900

    웹크롤링  250612
    멜론 최신곡 중
    순위 / 아티스트 / 노래제목 / 앨범명
    을 크롤링해서
    엑셀로 저장하는 코드
    아직 미완성

--- WebCrawling-1.py
+++ WebCrawling-1.py
@@ -18,9 +18,9 @@ for Memo in NewList50:
     Title = Memo.select_one('div > div.ellipsis.rank01 > span > a')
 
     if Title is not None:
-        artist = Memo.select_one('div.ellipsis.rank02 > a').text
+        Artist = Memo.select_one('div.ellipsis.rank02 > a').text
         Album = Memo.select_one('div.ellipsis.rank03 > a')
-        print(artist, ',', Title.text, ',', Album)
+        print(Artist, ',', Title.text, ',', Album)
 
         wb = Workbook()
         ws = wb.active
@@ -29,5 +29,21 @@ for Memo in NewList50:
         ws['A1'] = '멜론최신곡50'
         column = ['순위','아티스티','노래제목','앨범명']
         ws.append(column)
-        row = [Rank,artist,Title,Album]
-        
+        row = [Rank,Artist,Title,Album]
+        prefix1 = 'A'
+        prefix2 = 'B'
+        prefix3 = 'C'
+        prefix4 = 'D'
+
+        num1 = 3
+        while num1 < 53 :
+            str1 = f'{prefix1}{num1}'
+            str2 = f'{prefix2}{num1}'
+            str3 = f'{prefix3}{num1}'
+            str4 = f'{prefix4}{num1}'
+            ws[str1] = Rank
+            ws[str2] = Artist
+            ws[str3] = Title
+            ws[str4] = Album
+            print(ws[str1],ws[str2],ws[str3],ws[str4])
+            num1 = num1 + 1
