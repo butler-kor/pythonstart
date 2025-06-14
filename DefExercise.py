47c1a080e1349fe897819ec5664fc52eec115ffe
butler-kor <youinmok@daum.net>
2025-06-14 21:35:29 +0900

    def 연습  250614
    223번 하나의 리스트를 받아서
    짝수만 화면에 출력하는 함수
    
    224번 하나의 딕셔너리의 key값만
    화면에 출력하는 함수

--- DefExercise.py
+++ DefExercise.py
@@ -292,3 +292,26 @@ from zstandard.backend_cffi import STRATEGY_BTLAZY2
 #     return(url)
 #
 # print(make_url("naver"))
+
+
+#223번 하나의 리스트를 입력받아 짝수만 화면에 출력하는 print_even 함수를 정의하라.
+
+# def print_even(List):
+#     List1 = []
+#     for i in List:
+#         if i % 2 == 0 :
+#             List1.append(i)
+#     return List1
+#
+# print(print_even([1, 3, 2, 10, 12, 11, 15]))
+
+
+# 224번 하나의 딕셔너리를 입력받아 딕셔너리의 key 값을 화면에 출력하는 print_keys 함수를 정의하라.
+
+# def print_keys(keys):
+#     for key in keys.keys():
+#         print(key)
+#
+#
+# dic1 =  ({"이름":"김말똥", "나이":30, "성별":0})
+# print_keys(dic1)
