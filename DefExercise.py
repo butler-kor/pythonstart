d5a6fc01867207866bc67e4286694871ec59ff0b
butler-kor <youinmok@daum.net>
2025-07-01 21:19:37 +0900

    함수 연습 250701
    243번 오늘기준 특정 날짜 출력
    틀렸음

--- DefExercise.py
+++ DefExercise.py
@@ -452,3 +452,14 @@ from zstandard.backend_cffi import STRATEGY_BTLAZY2
 # import datetime
 # NowTime = datetime.datetime.now()
 # print(NowTime,type(NowTime))
+
+# 243번 datetime 모듈의 timedelta를 사용해서 오늘로부터 5일, 4일, 3일, 2일, 1일 전의 날짜를 화면에 출력해보세요.
+
+# import datetime
+# now = datetime.datetime.now()
+# for day in range(5, 0, -1):
+#     delta = datetime.timedelta(days=day)
+#     date = now - delta
+#     print(date)
+#  # 틀렸음
+
